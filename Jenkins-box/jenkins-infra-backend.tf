322de852e05d157484cd83addd2bb718dc58e32c
Deji Oresanwo <deji.oresanwo@gmail.com>
2020-07-05 01:21:49 +0100

    Backend updated

--- Jenkins-box/jenkins-infra-backend.tf
+++ Jenkins-box/jenkins-infra-backend.tf
@@ -1,9 +1,7 @@
-/*
 terraform {
   backend "s3" {
-    bucket_name = "weatherappdemo-terraform"
+    bucket = "weatherappdemo-terraform"
     key         = "jenkins-terraform-tfstate"
     region      = "eu-west-2"
   }
 }
-*/
