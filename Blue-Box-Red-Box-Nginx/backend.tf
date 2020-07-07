e9c21fd6df252eb43d3146bb52e4097c453254de
Deji Oresanwo <deji.oresanwo@gmail.com>
2020-07-08 00:41:18 +0100

    Withdrew apps layer config files

--- Blue-Box-Red-Box-Nginx/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "weatherappdemo-terraform"
-    key    = "app-layer-tfstate"
-    region = "eu-west-2"
-  }
-}
--- Jenkins-box/jenkins-infra-backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "weatherappdemo-terraform"
-    key         = "jenkins-terraform-tfstate"
-    region      = "eu-west-2"
-  }
-}
